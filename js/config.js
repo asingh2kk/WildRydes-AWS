00d5e89eb8f0474c06bd1b84780504d2ba224485
akasingh15 <148171368+akasingh15@users.noreply.github.com>
2024-01-25 23:23:20 -0500

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+  cognito: {
+    userPoolId: "us-east-2_GVHNj5Jxi", // e.g. us-east-2_uXboG5pAb
+    userPoolClientId: "6er5b8l7c3ofd7d506f6v70n4h", // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+    region: "us-east-2", // e.g. us-east-2
+  },
+  api: {
+    invokeUrl: "", // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+  },
+};
